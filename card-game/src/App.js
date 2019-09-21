631f239d07c32c157c79e50077e39391abcb1f67
runchida <6010110300@psu.ac.th>
2019-09-21 20:23:58 +0700

    react lab

---
+++ card-game/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import WordCard from './WordCard';
+//import CharacterCard from './CharacterCard';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <WordCard value="LOVE"/>
+
+
+
+
+            </div>
+        );
+    }
+}
+export default App;
